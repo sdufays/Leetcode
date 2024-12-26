cea6ce2c7af7e716051a8db003ab19703400adb6
Sarah <sdufays@mit.edu>
2024-12-26 17:52:51 -0500

    Update Arrays-and-Hashing.py

--- Arrays-and-Hashing.py
+++ Arrays-and-Hashing.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
+from collections import Counter
 from typing import List
+import heapq
 
 """
 Python Notes:
@@ -10,7 +12,12 @@ sorted(word) returns list of every character in sorted order
 "".join(sorted(word)) returns 1 string with characters in sorted order
 defaultdict(default) is a subclass of Python's built-in dict that automatically provides a default value for keys that don't exist
 --> need * from collections import defaultdict * , can pass in list to init with empty lists or ints to init. with zero's
-tuples are immutable, can use to pass in for keys of list
+tuples are immutable, can use to pass in lists for keys of a dictionary
+count[ord(char) - ord('a')] maps the character to an index in the range [0, 25] where 0 is a and 25 is z
+can use Counter (from collections) to count the occurrences of each number/char in a list
+.items() for when you want to iterate over both the keys and values in a dictionary
+can use bucket sort when you need frequencies 
+heaq gives you a min heap --> if you want a max heap, do heapq.nlargest(n, iterable, key=None) to get n largest vals
 
 """
 class Solution:
@@ -52,15 +59,41 @@ class Solution:
             result[key].append(word)
         return list(result.values())
     #o(n * k log k) runtime and o(nk) memory
-    def groupAnagrams_BETTER(self, strs: List[str]) -> List[List[str]]:
+    
+    #medium, better implementation
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # n words, k average length
         result = defaultdict(list)
         for word in strs:
-            count = [0] * 26
+            count = [0] * 26 # o(1) memory
             for char in word:
                 count[ord(char) - ord('a')] += 1
             result[tuple(count)].append(word)
         return list(result.values())
+    #o(n*k) for runtime and memory
+
+    #medium
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # bucket sort, this approach is better when k << n
+        frequency = Counter(nums) # o(n) time and space
+        n = len(nums)
+        buckets = [[] for _ in range(n+1)] # o(n) space
+        for num, freq in frequency.items(): #o(n) time
+            buckets[freq].append(num)
+        top_k= [] 
+        for freq in range(n,0,-1): # o(n) time
+            for num in buckets[freq]:
+                top_k.append(num)
+                if len(top_k) == k:
+                    return top_k
+        return top_k
+    #o(n) runtime and memory
+
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # max heap, better when k !<< n
+        frequency = Counter(nums)
+        return [num for num, _ in heapq.nlargest(k, frequency.items(), key=lambda x: x[1])]
+    #o(n log k), o(n) memory
 
 
         
